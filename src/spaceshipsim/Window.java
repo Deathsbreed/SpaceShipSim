b0360f73089f0df0efa3d306dbac81099961f4ed
Deathsbreed <nicolas.ortega.froysa@gmail.com>
2014-05-27 20:43:58 -0500

    Added new not-finished class for About window.

---
+++ src/spaceshipsim/Window.java
@@ -0,0 +1,15 @@
+package spaceshipsim;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Nicolás A. Ortega
+ * @copyright Nicolás A. Ortega
+ * @license GNU GPLv3
+ * @year 2014
+ * 
+ */
+public class Window {
+	public 
+}
